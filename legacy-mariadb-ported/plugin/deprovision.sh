d9fbf44c3af501844300dc0a65621e7a1390c958
Daniel De Vera <daniel@signadot.com>
2023-03-14 09:31:55 -0300

    Make legacy-mariadb-ported a single file plugin

--- legacy-mariadb-ported/plugin/deprovision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# exit when any command fails
-set -e
-
-/signadot/plugin/bin/deprovision ${SIGNADOT_SANDBOX_ID} ${SIGNADOT_RESOURCE_NAME}
--- legacy-mariadb-ported/plugin/provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# exit when any command fails
-set -e
-
-mkdir -p /signadot/plugin/output
-/signadot/plugin/bin/provision ${SIGNADOT_SANDBOX_ID} ${SIGNADOT_RESOURCE_NAME}
