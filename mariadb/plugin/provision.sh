0e1e9ab91d08371ab233f12fa853d74eada40a99
arjun-sd <57191194+arjun-sd@users.noreply.github.com>
2026-01-15 15:10:19 -0800

    fix(mariadb): update chart to v24.0.3 and fix RBAC permissions

--- mariadb/plugin/provision.sh
+++ mariadb/plugin/provision.sh
@@ -9,19 +9,18 @@ echo "DB name: ${DBNAME}"
 
 # Install bitnami helm chart repo
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update bitnami
 
 # Deploy a temporary DB for this Sandbox
 export NAMESPACE=signadot
 RELEASE_NAME="signadot-${SIGNADOT_RESOURCE_NAME,,}-${SIGNADOT_SANDBOX_ID}"
 echo "Installing Helm release: ${RELEASE_NAME}"
 helm -n ${NAMESPACE} install "${RELEASE_NAME}" bitnami/mariadb \
---version 11.5.0 --wait --timeout 5m0s \
---set fullnameOverride="${RELEASE_NAME}-mariadb" \
---set primary.persistence.enabled=false \
---set serviceAccount.create=false \
---set auth.database="${DBNAME}" \
---set global.imageRegistry=docker.io \
---set global.repository=bitnamilegacy
+  --version 24.0.3 --wait --timeout 5m0s \
+  --set fullnameOverride="${RELEASE_NAME}-mariadb" \
+  --set primary.persistence.enabled=false \
+  --set serviceAccount.create=false \
+  --set auth.database="${DBNAME}"
 
 # Get the generated password, based on instructions from the Helm chart.
 MYSQL_ROOT_PASSWORD=$(kubectl -n ${NAMESPACE} get secret "${RELEASE_NAME}-mariadb" -o jsonpath="{.data.mariadb-root-password}" | base64 -d)
